e29bec9b6c2aed1414c9192ecb65a7352d6fd7f4
Toby Lelean <t.lelean@gmail.com>
2026-02-12 15:25:14 +0000

    Added; local versions of .js import scripts and changes to trim_csv

--- utils/trim_csv.py
+++ utils/trim_csv.py
@@ -1,6 +1,7 @@
 import pandas as pd
+from visualisation.backend.config import HISTORICAL_CSV
 
-def trim_csv(path="/var/opt/codesys/PlcLogic/trend_data/historical.csv", max_rows=30000):
+def trim_csv(path=HISTORICAL_CSV, max_rows=30000):
     df = pd.read_csv(path, header=None)
     if len(df) > max_rows:
         df.tail(max_rows).to_csv(path, index=False, header=False)
